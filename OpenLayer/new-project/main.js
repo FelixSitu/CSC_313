5ada9cc8a23ef5e4dd9bfe271bf1d314f1472202
FelixSitu <chsitu@calpoly.edu>
2020-04-13 12:29:30 -0700

    Added the main and index.htm

---
+++ OpenLayer/new-project/main.js
@@ -0,0 +1,42 @@
+var map = new ol.Map({
+  target: 'map',
+  layers: [
+    new ol.layer.Tile({
+      source: new ol.source.OSM()
+    })
+  ],
+  view: new ol.View({
+    center: ol.proj.fromLonLat([-120.6625, 37.41]),
+    zoom: 4
+  })
+});
+
+function add_map_point(lat, lng, radius, color) {
+  var layer = new ol.layer.Vector({
+      source: new ol.source.Vector({
+          features: [
+              new ol.Feature({
+                  geometry: new ol.geom.Point(ol.proj.fromLonLat([lat, lng])),
+              })
+          ]
+      }),
+      style: new ol.style.Style({
+        image: new ol.style.Circle({
+            radius: radius * 5,
+            fill: new ol.style.Fill({color: color})
+        })
+    })
+  });
+       
+  map.addLayer(layer);
+  
+}
+
+//San Francisco
+add_map_point(-122.727803, 37.785221, 1, 'red')
+
+//Los Angeles
+add_map_point(-118.2437, 34.0522, 2, 'blue')
+
+//San Luis Obispo
+add_map_point(-120.672788, 35.273149, 2, 'green')
